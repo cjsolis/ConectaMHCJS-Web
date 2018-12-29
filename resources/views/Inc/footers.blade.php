d6be5705edbb12db23f3134d9a471c715b32671c
stivenrojas <stevenrojasch@gmail.com>
2018-12-29 00:28:12 -0600

    Se arregla error del footer

---
+++ resources/views/Inc/footers.blade.php
@@ -0,0 +1,39 @@
+
+<div id="footer" >
+   <div class="container footer">
+     <div class="one_fourth">
+
+
+
+        <ul class="list-inline">
+          <li><a href="https://www.facebook.com/"><img src="/images/fbicon.gif" ></a></li>
+
+        </ul>
+
+
+       <ul>
+         <h5>Museo Histórico Cultural Juan Santamaría</h5>
+         <h5>Tel: (506) 2222-2222 / 2222-2224</h5>
+         <h5>museo@dominio.com</h5>
+
+       </ul>
+     </div>
+     <div class="one_fourth"></div>
+     <div class="one_fourth">
+       <ul>
+         <li class="lines"><a href="#" class="">Transparencia</a></li>
+         <li class="lines"><a href="#" class="">Trámites</a></li>
+         <li class="lines"><a href="#" class="">Preguntas Frecuentes</a></li>
+       </ul>
+     </div>
+     <div class="one_fourth lastcolumn">
+       <ul>
+
+         <li class="lines"><a href="#" class="">Denuncias y Quejas</a></li>
+         <li class="lines"><a href="#" class="">Enlaces Amigos</a></li>
+         <li class="lines"><a href="#" class="">Acerca de Nosotros</a></li>
+       </ul>
+     </div>
+   </div>
+   <p style="text-align: center;">Copyright 2014 &copy; Museo Histórico Cultural Juan Santamaría. Todos los derechos reservados</p>
+ </div>
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -12,59 +12,26 @@
   </head>
 
   <body>
-
-    <div class="page-wrap">
-        @include('inc.navbar')
-        <div class="container">
-          @include('inc.showcase')
-          <!--@if(Request::is('/'))
+    <div id="container">
+      <div class="page-wrap">
+          @include('inc.navbar')
+          <div class="container">
             @include('inc.showcase')
-          @endif-->
-           <div class="row" style=" position: absolute;  left: 200px;top: 350px;">
-             <div class="col-md-8 col-lg-8">
-               @include('inc.messages')
-               @yield('content')
+            <!--@if(Request::is('/'))
+              @include('inc.showcase')
+            @endif-->
+             <div class="row" style=" position: absolute;  left: 200px;top: 350px;">
+               <div class="col-md-8 col-lg-8">
+                 @include('inc.messages')
+                 @yield('content')
+               </div>
              </div>
-           </div>
-          </div>
+            </div>
+       </div>
      </div>
+@include('inc.footers')
 
 
-
-     <div id="footer">
-        <div class="container footer">
-          <div class="one_fourth">
-            <h3>Latest Tweets</h3>
-            <ul>
-              <h5>Museo Histórico Cultural Juan Santamaría</h5>
-              <h5>Tel: (506) 2222-2222 / 2222-2224</h5>
-              <h5>museo@dominio.com</h5>
-
-            </ul>
-          </div>
-          <div class="one_fourth">
-
-
-          </div>
-          <div class="one_fourth">
-
-            <ul>
-
-              <li class="lines"><a href="#" class="">Transparencia</a></li>
-              <li class="lines"><a href="#" class="">Trámites</a></li>
-              <li class="lines"><a href="#" class="">Preguntas Frecuentes</a></li>
-            </ul>
-          </div>
-          <div class="one_fourth lastcolumn">
-            <ul>
-
-              <li class="lines"><a href="#" class="">Denuncias y Quejas</a></li>
-              <li class="lines"><a href="#" class="">Enlaces Amigos</a></li>
-              <li class="lines"><a href="#" class="">Acerca de Nosotros</a></li>
-            </ul>
-          </div>
-        </div>
-      </div>
 
 
      <!--<footer>
