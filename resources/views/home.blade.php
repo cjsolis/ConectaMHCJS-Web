2981a9660d7248703ec17af49ca0c750ad13d69b
stivenrojas <stevenrojasch@gmail.com>
2019-01-05 18:25:35 -0600

    se configura para localhost

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,6 +1,6 @@
 @extends('layouts.app')
 @section ('content')
-@include('inc.carrousel')
+@include('Inc.carrousel')
 <div class="container" style="left: 200px;position: absolute; ">
   <iframe width="560" height="315" src="https://www.youtube.com/embed/cWB3mSspcgw" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
 </div>
