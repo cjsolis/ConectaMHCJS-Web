565d4985efd287a2f445febe55bfaa475d45e620
stivenrojas <stevenrojasch@gmail.com>
2019-01-17 11:15:43 -0600

    se actualiza la seccion de acerca de nosotros

--- resources/views/acerca.blade.php
+++ resources/views/acerca.blade.php
@@ -1,6 +1,23 @@
 @extends('layouts.app')
 @section ('content')
 <h1>Acerca de Nosotros</h1>
-<p>En construcción</p>
+<p ALIGN="justify">
+  El Museo Histórico Cultural Juan Santamaría fue creado mediante Ley de la República No. 5619, del 4 de diciembre de 1974, reformada por la Ley No. 6572 del 23 de abril de 1981, la cual cuenta con su respectivo Reglamento. El proyecto que dio origen a esta ley fue iniciativa del entonces diputado alajuelense Dr. Luis A. Salas Corrales (+).<br />
+El Museo lleva el nombre de “Juan Santamaría” en honor al Héroe Nacional, cuya gesta heroica se concreta el 11 de abril de 1856 en Rivas, Nicaragua. El 9 de abril de 1980, el Museo abrió sus puertas a la comunidad local y nacional.<br />
+Este Museo es el único en el país especializado en el tema de la Campaña Nacional de 1856-57 contra el invasor filibustero.<br /><br />
+  <b>Misión</b><br /><br />
+  Somos la institución creada para la recuperación, custodia, conservación y divulgación del patrimonio histórico de la gesta heroica de 1856-1857 para mantenerla vigente en la memoria colectiva de los costarricenses, por medio de diversas manifestaciones culturales y a la vez promover el desarrollo cultural alajuelense con fines educativos y de deleite.<br />
+<br/>
+  <b>Visión</b><br />Ser una institución posicionada a nivel nacional, que impulsa, sensibiliza y fortalece la identidad histórica del costarricense respecto a la Campaña Nacional y del Alajuelense en relación con su patrimonio cultural. <br/>
+
+<br/>
+  <b>Organización</b><br />El Museo es una institución adscrita al Ministerio de Cultura y Juventud. La estructura organizativa para la atención de los servicios y labores que tiene a cargo el Museo está fundamentada en una Junta Administrativa, la Dirección General, el Área de Servicios Museológicos y el Área Administrativa. <br/>
+  La Junta administrativa conformada por cinco miembros rige la institución, aunada a la Dirección General que guía y lidera las labores institucionales.
+  El área de Servicios Museológicos atiende aspectos relativos a colecciones, documentación museográfica, conservación, exhibiciones, educación, promoción cultural y centro de información. El Área Administrativa funciona como apoyo a las labores que se realizan dentro de la institución.<br/>
+
+<br/>
+  <b>Voluntariado</b><br />Esta institución museística recibe con agrado a personas que deseen realizar voluntariado, así como trabajo comunal estudiantil y prácticas profesionales de diversas instituciones y especialidades. Para ser recibidos, el solicitante  debe presentar una carta de la institución educativa de donde proviene, donde solicite al Museo su aceptación.<br/>
+</p>
 
 @endsection
+
