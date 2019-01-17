c8df2c17e572e7b5525927f4c38189e1d8f7fad3
stivenrojas <stevenrojasch@gmail.com>
2019-01-17 11:19:23 -0600

    se actualiza la seccion de acerca de nosotros

--- resources/views/acerca.blade.php
+++ resources/views/acerca.blade.php
@@ -19,5 +19,14 @@ Este Museo es el único en el país especializado en el tema de la Campaña Naci
   <b>Voluntariado</b><br />Esta institución museística recibe con agrado a personas que deseen realizar voluntariado, así como trabajo comunal estudiantil y prácticas profesionales de diversas instituciones y especialidades. Para ser recibidos, el solicitante  debe presentar una carta de la institución educativa de donde proviene, donde solicite al Museo su aceptación.<br/>
 </p>
 
+<br/><br/>
+
+<b>Arquitectura</b><br />
+El Museo tiene su sede en los edificios conocidos como “Antigua Cárcel de Alajuela”, una edificación construida entre 1894 y 1895 y “Antiguo Cuartel de Armas de la ciudad de Alajuela, también edificado en el siglo XIX. Los dos edificios fueron declarados de interés histórico – patrimonial a través del decreto No. 9951-C del 5 de marzo de 1979 y decreto No.6358 del 9 de setiembre de 1976.
+El primer antecedente registral de la finca donde están los dos edificios se encuentra en el protocolo colonial de Cartago No. 1050, con fecha 14 de noviembre de 1817. A finales de setiembre de 1822, don Rosario Carrillo y su esposa doña María Fernández donaron a la comunidad el terreno donde está el edificio de  la antigua cárcel. Su voluntad era que se construyera una casa de enseñanza y una ermita a San Miguel Arcángel, pero lo que se construyó fue la cárcel, que funcionó hasta 1974.
+ <br /><br />
+El antiguo cuartel fue construido durante el gobierno de Tomás Guardia, entre 1874 y 1877 y remodelado en 1936. En 1949 al abolirse el ejército como institución permanente, el antiguo cuartel de armas de Alajuela fue ocupado a partir de 1960 por el Colegio vocacional Jesús Ocaña. Luego, a partir de 1977 pasó a ser sede del Centro de Investigación para el Perfeccionamiento Técnico (CIPET), una institución del Ministerio de Educación Pública.
+ <br /><br />
+Dentro de los logros más relevantes se encuentra la ley presentada por el diputado alajuelense, Dr. Eduardo Soto Chavarría, ante la Comisión Permanente de Asuntos Económicos de la Asamblea Legislativa, relativo al traspaso del “Antiguo Cuartel de Alajuela” al Museo Histórico Cultural Juan Santamaría, el cual tuvo un rápido dictamen afirmativo de mayoría el 14 de octubre de 1988. Por Ley N° 7895, del 30 de julio de 1999 se autorizó el traspaso al Museo del edificio para uso específico de los fines y misión encomendados a esta institución.
 @endsection
 
