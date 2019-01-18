d95b5f317270e1a18cd26ca59e3d315083be5aab
Christopher Solis <cjsolis_07@hotmail.com>
2019-01-18 12:15:22 -0600

    Routes change

---
+++ database/seeds/AdminTableSeeder.php
@@ -0,0 +1,22 @@
+<?php
+
+use Illuminate\Database\Seeder;
+use App\Admin;
+
+class AdminTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        Admin::create([
+            'name' => 'Admin',
+            'email' => 'admin@admin.com',
+            'password' => Hash::make('password'),
+            'remember_token' => str_random(10),
+          ]);
+    }
+}
