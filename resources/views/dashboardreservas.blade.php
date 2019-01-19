69c4d5ac7c4805d2c94c410f1d19a727300014fd
stivenrojas <stevenrojasch@gmail.com>
2019-01-19 12:02:53 -0600

    implementacion de dashboard de reservas del usuario arreglado

--- resources/views/dashboardreservas.blade.php
+++ resources/views/dashboardreservas.blade.php
@@ -1,108 +1,12 @@
-@extends('layouts.dashboardreservaslayout')
+@extends('layouts.app')
 @section ('content')
-<style>
-    /* NOTE: The styles were added inline because Prefixfree needs access to your styles and they must be inlined if they are on local disk! */
-    @import url("http://fonts.googleapis.com/css?family=Open+Sans:400,600,700");
-    @import url("http://netdna.bootstrapcdn.com/font-awesome/4.1.0/css/font-awesome.css");
-
-    *, *:before, *:after {
-        margin: 0;
-        padding: 0;
-        box-sizing: border-box;
+<style media="screen">
+    .ignoreparentreserva {
+        width: 100vw;
+        margin-left: calc(65% - 50vw);
     }
-
-
-    h1 {
-        padding: 50px 0;
-        font-weight: 400;
-        text-align: center;
-    }
-
-    p {
-        margin: 0 0 20px;
-        line-height: 1.5;
-    }
-
-    main {
-        min-width: 920px;
-        max-width: 900px;
-        padding: 50px;
-        margin: 0 auto;
-        background: #fff;
-    }
-
-    section {
-        display: none;
-        padding: 20px 0 0;
-        border-top: 1px solid #ddd;
-    }
-
-    .custom-radio{
-        display:none;
-    }
-
-    .custom-radio:checked + label {
-        color: #555;
-        border: 1px solid #ddd;
-        border-top: 2px solid black;
-        border-bottom: 1px solid #fff;
-    }
-
-    .custom-label {
-        display: inline-block;
-        margin: 0 0 -1px;
-        padding: 15px 25px;
-        font-weight: 600;
-        text-align: center;
-        color: #bbb;
-        border: 1px solid transparent;
-    }
-
-    .custom-label:before {
-        font-family: fontawesome;
-        font-weight: normal;
-        margin-right: 10px;
-    }
-
-
-    .custom-label:hover {
-        color: #888;
-        cursor: pointer;
-    }
-
-    #tab1:checked ~ #content1,
-    #tab2:checked ~ #content2 {
-        display: block;
-    }
-
-    .custom-textfield{
-        width: 100%;
-        font-size:18px;
-    }
-
-    table {
-      font-family: arial, sans-serif;
-      border-collapse: collapse;
-      width: 100%;
-    }
-
-    td, th {
-      border: 1px solid #dddddd;
-      text-align: left;
-      padding: 8px;
-    }
-
-    tr:nth-child(even) {
-      background-color: #dddddd;
-    }
-
 </style>
 
-
-
-
-
-<script src="https://cdnjs.cloudflare.com/ajax/libs/prefixfree/1.0.7/prefixfree.min.js"></script>
 
 <?php
 $userId = Auth::id();//id unico del usuario logueado.
@@ -113,39 +17,39 @@ $datoporidusuario = App\User::find($userId);
 //$formularios = App\ReservaVisitaGuiada::orderBy('id_reserva', 'desc')->where('id_usuario','=',$userId);
 //echo $datoporidusuario
 //echo $datostablareservas
-
 ?>
 
-
+<div class="ignoreparentreserva">
 @if(count($datostablareservas)>0)
     <table>
       <tr>
-        <th>Número reserva</th>
-        <th>Institución</th>
-        <th>Personas</th>
-        <th>Rango de edad</th>
-        <th>Fecha</th>
-        <th>Hora</th>
-        <th>Materiales Educativos</th>
-        <th>Necesidades Especiales</th>
-        <th>Teléfono</th>
+        <th>&nbsp Número reserva &nbsp</th>
+        <th>&nbsp Institución &nbsp</th>
+        <th>&nbsp Personas &nbsp</th>
+        <th>&nbsp Rango de edad &nbsp</th>
+        <th>&nbsp Fecha &nbsp  </th>
+        <th>&nbsp Hora &nbsp </th>
+        <th>&nbsp Materiales Educativos  &nbsp</th>
+        <th>&nbsp Necesidades Especiales &nbsp </th>
+        <th>&nbsp Teléfono  &nbsp</th>
 
         </tr>
   @foreach($datostablareservas as $datos)
       <tr>
         <td>{{$datos->id_reserva}}</td>
-        <td>{{$datos->institucion}}</td>
-        <td>{{$datos->numpersonas}}</td>
-        <td>{{$datos->rangoedad}}</td>
-        <td>{{$datos->fecha}}</td>
-        <td>{{$datos->hora}}</td>
-        <td>{{$datos->materialeseducativos}}</td>
-        <td>{{$datos->necesidadesespeciales}}</td>
-        <td>{{$datos->telefono}}</td>
+        <td>&nbsp {{$datos->institucion}} &nbsp</td>
+        <td> {{$datos->numpersonas}}</td>
+        <td>&nbsp {{$datos->rangoedad}} &nbsp</td>
+        <td>&nbsp {{$datos->fecha}} &nbsp</td>
+        <td>&nbsp {{$datos->hora}} &nbsp</td>
+        <td>&nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp{{$datos->materialeseducativos}} </td>
+        <td>&nbsp&nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp{{$datos->necesidadesespeciales}} </td>
+        <td>&nbsp {{$datos->telefono}}&nbsp </td>
       </tr>
   @endforeach
   </table>
 @else
     No hay reservas para este usuario.
 @endif
+</div>
 @endsection
