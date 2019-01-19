86a96a9d6b5d00e1401216071d3589c994c9df95
stivenrojas <stevenrojasch@gmail.com>
2019-01-19 15:16:40 -0600

    correccion catalogo

--- resources/views/catalogo.blade.php
+++ resources/views/catalogo.blade.php
@@ -1,5 +1,63 @@
 @extends('layouts.app')
 @section ('content')
-<h1>Catálogo</h1>
-<p>En construcción.</p>
+
+<style>
+.grid-container {
+  display: grid;
+  grid-template-columns: auto auto auto;
+  background-color: white;
+  padding: 10px;
+  width:100%;
+}
+.grid-item {
+  background-color: rgba(255, 255, 255, 0.8);
+  border: 1px solid rgba(0, 0, 0, 0.8);
+  padding: 20px;
+  font-size: 30px;
+  text-align: center;
+}
+.item-pieza{
+  width:100%;
+}
+</style>
+
+
+
+<h1>Catálogo de Piezas</h1>
+
+<div class="grid-container">
+  <div class="grid-item">
+    <strong>Titulo de pieza</strong>
+    <div class="item-pieza">
+      <img src="/images/carrouselinicio/banner-1.jpg" alt="Imagen1" >
+    </div>
+  </div>
+  <div class="grid-item">
+    <strong>Titulo de pieza</strong>
+    <div class="item-pieza">
+      <img src="/images/carrouselinicio/banner-1.jpg" alt="Imagen1" >
+    </div>
+  </div>
+  <div class="grid-item">
+    <strong>Titulo de pieza</strong>
+    <div class="item-pieza">
+      <img src="/images/carrouselinicio/banner-1.jpg" alt="Imagen1">
+    </div>
+  </div>
+  <div class="grid-item">
+    <strong>Titulo de pieza</strong>
+    <div class="item-pieza">
+      <img src="/images/carrouselinicio/banner-1.jpg" alt="Imagen1">
+    </div>
+  </div>
+  <div class="grid-item">2</div>
+  <div class="grid-item">3</div>
+  <div class="grid-item">4</div>
+  <div class="grid-item">5</div>
+  <div class="grid-item">6</div>
+
+</div>
+
+
+
 @endsection
